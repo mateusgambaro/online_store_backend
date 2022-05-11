7274d34e61517de7ec13f11e438db116ae6f7d58
mateusgambaro <magambaro@hotmail.com>
2022-05-11 08:24:42 -0300

    try again

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const cors = require('cors')
 
-const userController = require('./controllers/userController');
+const userController = require('./controllers/UserController');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.DB_PORT;
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/', userController);
+app.get('/', async (_req, res) => {
+    try {
+      const users = await User.findAll();
+  
+      return res.status(200).json(users);
+    } catch (e) {
+      console.log(e.message);
+      res.status(500).json({ message: 'Algo deu errado' });
+    };
+  });
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
