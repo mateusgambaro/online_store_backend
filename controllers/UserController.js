f43f83d6f6bae2e98800881e1c333648c315dc09
mateusgambaro <magambaro@hotmail.com>
2022-05-11 08:27:45 -0300

    try 2

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const  { User }  = require('../models/User');
+const { User } = require('../models');
 const router = express.Router();
 
-// router.get('/', async (_req, res) => {
-//   try {
-//     const users = await User.findAll();
+router.get('/', async (_req, res) => {
+  try {
+    const users = await User.findAll();
 
-//     return res.status(200).json(users);
-//   } catch (e) {
-//     console.log(e.message);
-//     res.status(500).json({ message: 'Algo deu errado' });
-//   };
-// });
+    return res.status(200).json(users);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  };
+});
 
 
 // router.get('/:id', async (req, res) => {
--- index.js
+++ index.js
@@ -5,20 +5,11 @@ const cors = require('cors')
 const userController = require('./controllers/UserController');
 
 const app = express();
-const PORT = process.env.DB_PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', async (_req, res) => {
-    try {
-      const users = await User.findAll();
-  
-      return res.status(200).json(users);
-    } catch (e) {
-      console.log(e.message);
-      res.status(500).json({ message: 'Algo deu errado' });
-    };
-  });
+app.use('/', userController);
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
